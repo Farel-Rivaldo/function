ad0a3a7e7ddf03ab00755380762ed57f1da3eeea
Farel-Rivaldo <ipalgaming90@gmail.com>
2023-03-10 13:13:54 +0700

    javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function luas(r) {
+    const phi = 22/7
+    L = phi * (r * r)
+    return L
+}
+
+let r = 14
+let luaslingkaran = luas(r)
+console.log('Luas: ' + luaslingkaran)
+document.writeln(L)
